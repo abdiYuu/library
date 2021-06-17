bad49d9ba56ef65988c18766060b8878320839d6
Abdi Yusuf <abdiyuu7@gmail.com>
2021-06-16 19:40:24 -0600

    started on basic html structure and library logic

--- app.js
+++ app.js
@@ -7,5 +7,25 @@ function Book(title, author, pages, read) {
 		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
 	}
 }
-const eloquentJS = new Book('Eloquent JS', 'Marijn Haverbeke', 463, 'currently reading');
-console.log(eloquentJS.info());
+
+
+function addBook(title, author, pages, read) {
+	books.push(new Book(title, author, pages, read));
+	console.table(books)
+}
+
+function removeBook(book) {
+	myBooks.splice(book, 1);
+}
+function showBook(book) {
+	//display book on a card on the page
+}
+
+const books = [];
+
+//make query listeners to add books to the page
+
+const btn = document.querySelector('.btn') 
+btn.addEventListener('click', addBook);
+
+
